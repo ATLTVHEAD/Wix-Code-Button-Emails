c27685fa15e325ed663226bd258300f2c307f7e1
ndamen3 <ndamen3@users.noreply.github.com>
2018-09-24 08:06:36 -0400

    Update Atltvhead_Gmail_Python_Trigger.py

--- Atltvhead_Gmail_Python_Trigger.py
+++ Atltvhead_Gmail_Python_Trigger.py
@@ -50,9 +50,15 @@ user_id = 'me'
 label_id_one = 'INBOX'
 label_id_two = 'UNREAD'
 
+
+
 #gets all of the unread messages from the inbox
 def remail():
+    t=0
     while True:
+        ot=time.time()
+        diff = ot - t
+        
         unread_msgs = service.users().messages().list(userId='me',labelIds=[label_id_two]).execute()
 
     
@@ -80,39 +86,63 @@ def remail():
                        temp_dict['Sender']= msg_from
                     else:
                        pass
-            #print(temp_dict['Subject'])
+            
             #print(temp_dict['Sender']) #should always look like this for me natedamen <nate.damen@pb08.wixshoutout.com>
                 
             if temp_dict['Subject'] == 'rnbw':
-                chat(s,"!rainbow")
+                chat(s,"!rainbowHeart")
+                #print(temp_dict['Subject'])
                 service.users().messages().modify(userId=user_id, id=m_id, body={ 'removeLabelIds': ['UNREAD']}).execute() 
 
             elif temp_dict['Subject'] == 'gltchd':
                 chat(s,"!flicker")
+                #print(temp_dict['Subject'])
                 service.users().messages().modify(userId=user_id, id=m_id, body={ 'removeLabelIds': ['UNREAD']}).execute()
 
             elif temp_dict['Subject'] == 'fxdgltch':
                 chat(s,"!flickerOff")
+                #print(temp_dict['Subject'])
                 service.users().messages().modify(userId=user_id, id=m_id, body={ 'removeLabelIds': ['UNREAD']}).execute()
 
             elif temp_dict['Subject'] == 'hrt':
                 chat(s,"!heartColor")
+                #print(temp_dict['Subject'])
                 service.users().messages().modify(userId=user_id, id=m_id, body={ 'removeLabelIds': ['UNREAD']}).execute()
 
             elif temp_dict['Subject'] == 'bgclr':
                 chat(s,"!backgroundColor")
+                #print(temp_dict['Subject'])
                 service.users().messages().modify(userId=user_id, id=m_id, body={ 'removeLabelIds': ['UNREAD']}).execute()
 
             elif temp_dict['Subject'] == 'rst':
                 chat(s,"!reset")
+                #print(temp_dict['Subject'])
                 service.users().messages().modify(userId=user_id, id=m_id, body={ 'removeLabelIds': ['UNREAD']}).execute()
 
             elif temp_dict['Subject'] == 'ch1':
-                chat(s,"!ch1")
+                chat(s,"<3")
+                #print(temp_dict['Subject'])
                 service.users().messages().modify(userId=user_id, id=m_id, body={ 'removeLabelIds': ['UNREAD']}).execute()
-                
-        time.sleep(1 / cfg.RATE)
-             
+            elif temp_dict['Subject'] == 'ch2':
+                chat(s,"!mirrorMirror")
+                #print(temp_dict['Subject'])
+                service.users().messages().modify(userId=user_id, id=m_id, body={ 'removeLabelIds': ['UNREAD']}).execute()
+            elif temp_dict['Subject'] == 'frainbow':
+                chat(s,"!fullRainbow")
+                service.users().messages().modify(userId=user_id, id=m_id, body={ 'removeLabelIds': ['UNREAD']}).execute()
+            elif temp_dict['Subject'] == 'sparkles':
+                chat(s,"!sparkles")
+                service.users().messages().modify(userId=user_id, id=m_id, body={ 'removeLabelIds': ['UNREAD']}).execute()
+
+            time.sleep(1 / cfg.RATE)
+            
+        if diff > 240:
+            t=time.time()
+            diff=0
+            ot=time.time()
+            chat(s,"Hey Chat! This is TvheadBot just letting you know a few commands: !rainbow     !ch1     !heart")
+          
+        #time.sleep(1 / cfg.RATE)     
 
 
 
@@ -121,15 +151,19 @@ def pingPong():
     while True:    
         response = s.recv(1024).decode("utf-8")
         if response == "PING :tmi.twitch.tv\r\n":
-            s.send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))       
-            
-    time.sleep(1 / cfg.RATE)
-
+            s.send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))
+            time.sleep(1 / cfg.RATE) 
+            print('sent')
+        print(response) 
+        
 
 
 
 if __name__ == '__main__' :
     p =  multiprocessing.Process(target=pingPong)
     m =  multiprocessing.Process(target=remail)
-    p.start()
     m.start()
+    print('Started Email Scanning')
+    p.start()
+    print('ping pong started')
+    
